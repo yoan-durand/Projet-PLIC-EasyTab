a8d76c32ef4fe9b7dce6b34d142e9a643a7c173f
yoan <durand.yoan@gmail.com>
2012-05-29 12:25:00 +0200

    add array convertnote

--- src/public/js/DrawNote.js
+++ src/public/js/DrawNote.js
@@ -1,11 +1,9 @@
-function PosNote (x, y, fret, string, duration, measure)
+function PosNote (x, y, fret, string)
 {
     this._x = x;
     this._y = y;
     this._fret = fret;
     this._string = string;
-    this._duration = duration;
-    this._measure = measure;
 }
 
 function TimeNote (posx, posy, width, height, listnote)
