fd0277ea973c485c0dd83ff5d0073c44511b496f
yoan durand <yoan durand>
2012-07-17 14:07:38 +0200

    fix bug

--- src/public/js/Event.js
+++ src/public/js/Event.js
@@ -230,7 +230,7 @@ $(document).ready(setTimeout(function(){
 		
 		$(".progress_bar img[id='m_"+ selected + "']").attr("src", "image/casebleue.png");
 		 
-		if ((selected >= 30) && (selected % 30 == 0) && vertic == false)
+		if ((selected >= 28) && (selected % 14 == 0) && vertic == false)
 		{
 			vertic = true;
 			$(".bar").scrollTo("img[id='m_"+ selected + "']", 0, {axis:'x'});
@@ -268,13 +268,12 @@ $(document).ready(setTimeout(function(){
             javascript:document.demo.Stop();
             javascript:document.demo.SetTime(0);
             $(".overflow_svg").css("overflow-y", "auto");
-            $(".overflow_measure").css("overflow-x", "auto");
             $("#back").attr("src", "image/playerback.png");
             $("#play").attr("src", "image/playerplay.png");
             $("#pause").attr("src", "image/playerpause.png");
             $(this).attr("src", "image/playerstop2.png");
             $(".overflow_svg").scrollTo( 0, 0, {axis:'y'});
-            $(".overflow_measure").scrollTo( 0, 0, {axis:'x'});
+            $(".bar").scrollTo( 0, 0, {axis:'x'});
             scroll = 280;
             sline = 0;
             if (selected != 0)
@@ -286,9 +285,15 @@ $(document).ready(setTimeout(function(){
             setTimeout(function (){
                 $("#stop").attr("src", "image/playerstop.png");
             }, 500);
+            
+            for (var j = 0; j < svg_inst.length; j++)
+            	{
+            	$($("rect[id='cursor_"+current_svg+"']"), svg_inst[j].root()).attr({"y": 20});
+    			$($("rect[id='cursor_"+current_svg+"']"), svg_inst[j].root()).animate({svgTransform: 'translate(0 0)'}, 0, 'linear');
+            	}
+            
 			/*$($("rect[id='cursor_"+current_svg+"']"), svg_inst[current_svg].root()).stop();*/
-			$($("rect[id='cursor_"+current_svg+"']"), svg_inst[current_svg].root()).attr({"y": 20});
-			$($("rect[id='cursor_"+current_svg+"']"), svg_inst[current_svg].root()).animate({svgTransform: 'translate(0 0)'}, 0, 'linear');
+			
         }
     });
 
