9910dea5bec8e4889f68bbc09ac2a64c46131450
yoan durand <yoan durand>
2012-07-17 17:00:48 +0200

    defilement click tenu sur les fleche

--- src/public/js/Event.js
+++ src/public/js/Event.js
@@ -453,6 +453,33 @@ $(document).ready(setTimeout(function(){
 		$(".bar").scrollTo($(".bar img[id='m_"+progress_scroll+"']"), 0, {axis:"x"});
 	});
 	
+	$(".left").mousehold(function ()
+			{
+				if (selected > progress_scroll)
+				{
+					progress_scroll = selected;
+				}
+				
+				if (progress_scroll - 1 >= 0)
+				{
+					progress_scroll -= 1;
+				}
+				
+				$(".bar").scrollTo($(".bar img[id='m_"+progress_scroll+"']"), 0, {axis:"x"});
+			});
+	
+	$(".right").mousehold(function ()
+	{
+		if (selected > progress_scroll)
+		{
+			progress_scroll = selected;
+		}
+		
+		progress_scroll += 1;
+		
+		$(".bar").scrollTo($(".bar img[id='m_"+progress_scroll+"']"), 0, {axis:"x"});
+	});
+	
 	console.log('event');
 }, 1000));
 
