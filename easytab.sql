2953e996bd77ea76515fa270dfa4f8a562f5c5ba
n <n@n.n>
2012-12-06 19:16:41 +0100

    mise à jour de la structure SQL

--- easytab.sql
+++ easytab.sql
@@ -7,19 +7,55 @@
 -- Version de PHP: 5.3.9
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+02:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+SET time_zone = "+00:00";
 
 --
 -- Base de données: `easytab`
 --
-CREATE DATABASE `easytab` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `easytab`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `auteurId` int(11) NOT NULL,
+  `texte` text NOT NULL,
+  `tablatureId` int(11) NOT NULL,
+  `date` bigint(14) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `auteurId` (`auteurId`),
+  KEY `tablatureId` (`tablatureId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=58 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `favoris`
+--
+
+CREATE TABLE IF NOT EXISTS `favoris` (
+  `userId` int(11) NOT NULL,
+  `tablatureId` int(11) NOT NULL,
+  PRIMARY KEY (`userId`,`tablatureId`),
+  KEY `tablatureId` (`tablatureId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `note`
+--
+
+CREATE TABLE IF NOT EXISTS `note` (
+  `userId` int(11) NOT NULL,
+  `tablatureId` int(11) NOT NULL,
+  `note` int(11) NOT NULL,
+  PRIMARY KEY (`userId`,`tablatureId`),
+  KEY `tablatureId` (`tablatureId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -37,12 +73,7 @@ CREATE TABLE IF NOT EXISTS `tablature` (
   `public` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
-
---
--- Contenu de la table `tablature`
---
-
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
 
 -- --------------------------------------------------------
 
@@ -57,48 +88,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(40) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `user`
 --
 
 INSERT INTO `user` (`id`, `dateInscription`, `login`, `password`) VALUES
-(1, -5694963725000, 'Admin', '3174685bb0ac5dfed704b7b41a5e44b713b759a1'),
+(1, 0, 'Admin', '3174685bb0ac5dfed704b7b41a5e44b713b759a1'),
 (2, 1341763667245, 'Fab', '03b8b8d1ba64d7498760a6b5e6cf197335d62ad7'),
 (3, 1341884838345, 'quidam', '279dc1e9d24616e6e4b553320688323cf8febf08');
-
-
---
--- Structure de la table `comment`
---
-
-CREATE TABLE IF NOT EXISTS `comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `auteurId` int(11) NOT NULL,
-  `tablatureId` int(11) NOT NULL,
-  `texte` text NOT NULL,
-  `date` bigint(14) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `auteurId` (`auteurId`),
-  KEY `tablatureId` (`tablatureId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `note`
---
-
-CREATE TABLE IF NOT EXISTS `note` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `tablatureId` int(11) NOT NULL,
-  `note` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `tablatureId` (`tablatureId`),
-  KEY `userId` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Contraintes pour les tables exportées
@@ -108,22 +107,25 @@ CREATE TABLE IF NOT EXISTS `note` (
 -- Contraintes pour la table `comment`
 --
 ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`tablatureId`) REFERENCES `tablature` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`auteurId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`auteurId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`tablatureId`) REFERENCES `tablature` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `favoris`
+--
+ALTER TABLE `favoris`
+  ADD CONSTRAINT `favoris_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `favoris_ibfk_2` FOREIGN KEY (`tablatureId`) REFERENCES `tablature` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Contraintes pour la table `note`
 --
 ALTER TABLE `note`
-  ADD CONSTRAINT `note_ibfk_2` FOREIGN KEY (`tablatureId`) REFERENCES `tablature` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `note_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `note_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `note_ibfk_2` FOREIGN KEY (`tablatureId`) REFERENCES `tablature` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Contraintes pour la table `tablature`
 --
 ALTER TABLE `tablature`
   ADD CONSTRAINT `tablature_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
